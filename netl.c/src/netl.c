6d4d9b05b660786ccc0b0ed7ffaf6281ecaa2a78
KaisarCode <kaisar@kaisarcode.com>
2026-09-24 02:55:52 -0300

    Fix netl CLI KCS indentation

--- netl.c/src/netl.c
+++ netl.c/src/netl.c
@@ -667,15 +667,23 @@ int main(int argc, char **argv) {
     int command_start;
     int i;
 
-    if (argc == 2 &&
-        (strcmp(argv[1], "-h") == 0 ||
-         strcmp(argv[1], "--help") == 0)) {
+    if (
+        argc == 2 &&
+        (
+            strcmp(argv[1], "-h") == 0 ||
+            strcmp(argv[1], "--help") == 0
+        )
+    ) {
         cli_help(argv[0]);
         return 0;
     }
-    if (argc == 2 &&
-        (strcmp(argv[1], "-v") == 0 ||
-         strcmp(argv[1], "--version") == 0)) {
+    if (
+        argc == 2 &&
+        (
+            strcmp(argv[1], "-v") == 0 ||
+            strcmp(argv[1], "--version") == 0
+        )
+    ) {
         printf(
             "netl build %llu\n",
             (unsigned long long)kc_netl_version()
