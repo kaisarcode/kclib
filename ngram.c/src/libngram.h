6df39e2b437e29f2ca3deb95e0e63421b8ed4cb3
KaisarCode <kaisar@kaisarcode.com>
2026-09-24 21:13:06 -0300

    Normalize ngram public traversal ABI

--- ngram.c/src/libngram.h
+++ ngram.c/src/libngram.h
@@ -22,17 +22,15 @@ extern "C" {
 #define KC_NGRAM_EABORT -2
 
 typedef struct {
-    const char *input;
-    size_t byte_start;
-    size_t byte_end;
-    size_t start;
-    size_t end;
-    size_t size;
+    const char *data;
+    size_t data_size;
+    size_t token_start;
+    size_t token_count;
 } kc_ngram_chunk_t;
 
 typedef struct {
-    size_t max_tokens;
-    size_t min_tokens;
+    const size_t *max_tokens;
+    const size_t *min_tokens;
     const char *separators;
 } kc_ngram_options_t;
 
@@ -41,10 +39,12 @@ typedef int (*kc_ngram_visit_fn)(
     void *userdata
 );
 
-kc_ngram_options_t kc_ngram_options_default(void);
-
-int kc_ngram_execute(const char *input, const kc_ngram_options_t *options,
-    kc_ngram_visit_fn visit, void *userdata, size_t *out_count);
+int kc_ngram_traverse(
+    const char *input,
+    const kc_ngram_options_t *options,
+    kc_ngram_visit_fn visit,
+    void *userdata
+);
 
 uint64_t kc_ngram_version(void);
 
